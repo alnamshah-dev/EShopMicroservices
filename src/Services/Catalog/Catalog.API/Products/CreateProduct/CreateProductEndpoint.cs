a006ef32e028a8abb542bb246527d92dc9092185
AbdulazizALNamshah <aboodalnemshah@gmail.com>
2025-07-26 04:15:29 +0300

    initial

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/Products", async (CreateProductRequest request, ISender Sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await Sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/Products/{response.Id}", response);
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create Product");
+        }
+    }
+}
