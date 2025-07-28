dd33d40d505f532ba92faefd4415889035c928de
AbdulazizALNamshah <abdula2i2namsha@gmail.com>
2025-07-29 02:35:30 +0300

    Implement Fluent Validation with MediatR in command operations(create,update and delete) to validate requests before they reach to actual  handler within Pipline Behavior.

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BuildingBlocks.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var ValidationResult=
+                await Task.WhenAll(validators.Select(x=>x.ValidateAsync(context,cancellationToken)));
+            var Failures=
+                ValidationResult
+                .Where(x=>x.Errors.Any())
+                .SelectMany(x=>x.Errors)
+                .ToList();
+            if (Failures.Any())
+                throw new ValidationException(Failures);
+            return await next();
+        }
+    }
+}
