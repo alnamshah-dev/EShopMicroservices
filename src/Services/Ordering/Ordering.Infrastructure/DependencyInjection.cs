5b5b13ae046a53ad02156f5643ce4ef469cf33dc
AbdulazizALNamshah <abdula2i2namsha@gmail.com>
2025-08-11 00:44:22 +0300

    Ordering Infrastructure Development (EF Core - SQL Server)

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -7,12 +7,13 @@ namespace Ordering.Infrastructure
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("Database");
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddScoped<ISaveChangesInterceptor,AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor,DispatchDomainEventsInterceptor>();
+            services.AddDbContext<ApplicationDbContext>((sp,options) =>
             {
-                options.AddInterceptors(new AuditableEntityInterceptor());
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
                 options.UseSqlServer(connectionString);
             });
-            
             //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
             return services;
         }
