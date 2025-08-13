051b04818c7684c4601d23a0a755f3a70a3b8c86
AbdulazizALNamshah <abdula2i2namsha@gmail.com>
2025-08-13 23:39:13 +0300

    Ordering.API Development exposing minimal API endpoints (Carter - REPR Pattern)

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,21 @@
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+namespace Ordering.API.Endpoints;
+public record DeleteOrderResponse(bool IsSuccess);
+public class DeleteOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{Id}", async(Guid Id,ISender Sender) =>
+        {
+            var result = await Sender.Send(new DeleteOrderCommand(Id));
+            var response=result.Adapt<DeleteOrderResponse>();
+            return Results.Ok(response);
+        })
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Order")
+            .WithDescription("Delete Order");
+    }
+}
